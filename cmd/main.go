5f83fef9a4d64ae12a76e11b2576d9968d85f8a6
SchevaPratama <schevaprtm15@gmail.com>
2024-03-18 20:59:22 +0700

    boilerplate

---
+++ cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"golang_socmed/internal/config"
+)
+
+func main() {
+	viperConfig := config.NewViper()
+	app := config.NewFiber(viperConfig)
+	db := config.NewDatabase(viperConfig)
+	aws := config.NewAws(viperConfig)
+	validate := config.NewValidator(viperConfig)
+	log := config.NewLogger(viperConfig)
+	config.Bootstrap(&config.BootstrapConfig{
+		App:      app,
+		DB:       db,
+		Config:   viperConfig,
+		Validate: validate,
+		Log:      log,
+		Aws:      aws,
+	})
+	webPort := viperConfig.GetInt("web.port")
+	err := app.Listen(fmt.Sprintf(":%d", webPort))
+	if err != nil {
+		log.Fatal("Failed to start server: %w \n", err)
+	}
+}
